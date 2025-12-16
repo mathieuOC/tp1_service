b4feea6836a38d03459ca49402fd4550c657f8d1
mathieu <Mathieu.ouellettecabanillas@gmail.com>
2025-12-15 22:35:59 -0500

    hickaripool

---
+++ src/main/java/cal/info/hickaripool.java
@@ -0,0 +1,46 @@
+package cal.info;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public final class Hickaripool {
+
+    private static final HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+
+
+        config.setJdbcUrl("jdbc:h2:file:./data/hautlesbas-db");
+        config.setDriverClassName("org.h2.Driver");
+        config.setUsername("sa");
+        config.setPassword("");
+
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(30_000);
+        config.setIdleTimeout(600_000);
+        config.setMaxLifetime(1_800_000);
+
+
+        config.setAutoCommit(false);
+        config.setPoolName("HautLesBasPool");
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    private Hickaripool() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void shutdown() {
+        dataSource.close();
+    }
+}
